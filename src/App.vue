6c5f2de9dd4efb301741a081f5754cd4db9b7b53
Bartosz Kwiatek <bkwiatek3@gmail.com>
2022-10-04 21:47:15 +0200

    feat: add icons

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,44 @@
-<script setup></script>
-
 <template>
-  <div id="app">app</div>
+  <div id="app"></div>
 </template>
+
+<script setup>
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHeart as faHeartOutline } from "@fortawesome/free-regular-svg-icons";
+import {
+  faBackwardStep,
+  faBars,
+  faForwardStep,
+  faHeart,
+  faPause,
+  faPlay,
+  faRepeat,
+  faReply,
+  faRotateRight,
+  faShareNodes,
+  faShuffle,
+  faVolumeHigh,
+  faVolumeLow,
+  faVolumeOff,
+} from "@fortawesome/free-solid-svg-icons";
+
+library.add(
+  faPause,
+  faPlay,
+  faForwardStep,
+  faBackwardStep,
+  faShareNodes,
+  faVolumeHigh,
+  faVolumeLow,
+  faVolumeOff,
+  faBars,
+  faReply,
+  faShuffle,
+  faRepeat,
+  faRotateRight,
+  faHeart,
+  faHeartOutline
+);
+</script>
 
 <style scoped></style>
