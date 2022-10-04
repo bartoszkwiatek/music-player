30169c9fa88762f898544599a525a849572683dc
Bartosz Kwiatek <bkwiatek3@gmail.com>
2022-10-04 20:54:34 +0200

    feat: add vuex store

---
+++ src/components/playlist/PlaylistView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="songs">
+    <div v-for="(song, index) in songs" :key="index">{{ song }}</div>
+  </div>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+import { songs } from "../../assets/songs";
+
+export default {
+  name: "PlaylistView",
+  components: {},
+  computed: {
+    songs() {
+      return this.$store.state.songs;
+    },
+  },
+  methods: {
+    ...mapMutations(["addSongs"]),
+  },
+  created() {
+    this.addSongs(songs);
+  },
+};
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,60 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    currentSongIndex: 0,
+    settings: {
+      volume: 50,
+      random: false,
+      repeat: false,
+      loop: false,
+    },
+    songs: [],
+  },
+  getters: {
+    nextSongIndex(state) {
+      const nextSongIndex = state.currentSongIndex + 1;
+      const lastSongIndex = state.songs.length - 1;
+      // return random song index
+      if (state.settings.random)
+        return Math.floor(Math.random() * state.songs.length);
+      // return first song index
+      if (nextSongIndex > lastSongIndex && state.settings.loop) return 0;
+      // return null (for UI)
+      if (nextSongIndex > lastSongIndex) return null;
+      // return next song index
+      return nextSongIndex;
+    },
+    previousSongIndex(state) {
+      const previousSongIndex = state.currentSongIndex - 1;
+      // return random song index
+      if (state.settings.random)
+        return Math.floor(Math.random() * state.songs.length);
+      // return last song index
+      if (previousSongIndex < 0 && state.settings.loop)
+        return state.songs.length - 1;
+      // return null (for UI)
+      if (previousSongIndex < 0) return null;
+      // return previous song index
+      return previousSongIndex;
+    },
+  },
+  mutations: {
+    addSongs(state, songs) {
+      state.songs = [...state.songs, ...songs];
+    },
+    playSong(state, songIndex) {
+      if (state.songs[songIndex] !== undefined)
+        state.currentSongIndex = songIndex;
+    },
+    setSettings(state, settings) {
+      state.settings = { ...state.settings, ...settings };
+    },
+    toggleFavourite(state, songIndex) {
+      state.songs.splice(songIndex, 1, {
+        ...state.songs[songIndex],
+        favourite: !state.songs[songIndex].favourite,
+      });
+    },
+  },
 });
