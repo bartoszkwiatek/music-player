e5cde561a7d79d835eac25478ad76ca7fa6d772e
Bartosz Kwiatek <bkwiatek3@gmail.com>
2022-10-05 22:09:47 +0200

    feat: polish topbar component

--- src/components/player/PlayerTopbar.vue
+++ src/components/player/PlayerTopbar.vue
@@ -1,24 +1,50 @@
 <template>
-  <div style="display: flex">
-    <FontAwesomeIcon icon="fa-shuffle" />
-    <FontAwesomeIcon icon="fa-repeat" />
-    <FontAwesomeIcon icon="fa-rotate-right" />
-    <div @click="openPlaylist">
-      <FontAwesomeIcon icon="fa-bars" />
-    </div>
+  <div class="top-bar">
+    <RoundButton
+      v-for="[setting, value] in settingArray"
+      :key="setting"
+      @click.native="toggleSetting(setting)"
+      :color="value ? 'white' : 'gray'"
+      :icon="settingIcon[setting]"
+      backgroundColor="transparent"
+      size="xl"
+    />
+    <RoundButton
+      @click.native="openPlaylist"
+      icon="fa-bars"
+      color="gray"
+      backgroundColor="transparent"
+      class="menu"
+      size="xl"
+    />
   </div>
 </template>
 
 <script>
 import { mapMutations } from "vuex";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import RoundButton from "../common/RoundButton.vue";
 
 export default {
   name: "PlayerTopbar",
-  components: { FontAwesomeIcon },
+  components: { RoundButton },
+  data() {
+    return {
+      settingIcon: {
+        random: "fa-shuffle",
+        repeat: "fa-repeat",
+        loop: "fa-rotate-right",
+      },
+    };
+  },
   computed: {
     settings() {
       return this.$store.state.settings;
+    },
+    settingArray() {
+      return Object.entries(this.settings).filter(
+        ([setting]) => setting !== "volume"
+      );
     },
   },
   methods: {
@@ -26,8 +52,32 @@ export default {
     openPlaylist() {
       this.$emit("open-playlist");
     },
+    toggleSetting(setting) {
+      this.setSettings({
+        [setting]: !this.settings[setting],
+      });
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.top-bar {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+  height: 65px;
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+}
+.settings {
+  display: flex;
+  align-items: center;
+}
+.menu {
+  position: absolute;
+  right: 20px;
+}
+</style>
