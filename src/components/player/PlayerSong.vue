ec02748d952e049b436743be7c16584b775d0521
Bartosz Kwiatek <bkwiatek3@gmail.com>
2022-10-06 00:10:38 +0200

    feat: style playerSong component

--- src/components/player/PlayerSong.vue
+++ src/components/player/PlayerSong.vue
@@ -1,7 +1,13 @@
 <template>
-  <div v-if="currentSong">
-    <h2>{{ currentSong.title }}</h2>
-    <h3>{{ currentSong.artist }}</h3>
+  <div
+    v-if="currentSong"
+    class="song"
+    :style="{ 'background-image': `url(${imageUrl})` }"
+  >
+    <div class="song-info">
+      <h2>{{ currentSong.artist }}</h2>
+      <h3>{{ currentSong.title }}</h3>
+    </div>
   </div>
 </template>
 
@@ -12,8 +18,46 @@ export default {
   name: "PlayerSong",
   computed: {
     ...mapGetters(["currentSong"]),
+    imageUrl() {
+      return `src/assets/img/${this.currentSong.img}`;
+    },
   },
 };
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+.song {
+  position: relative;
+  width: 100%;
+  height: 340px;
+  background-size: cover;
+
+  .song-info {
+    position: absolute;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+    height: 65px;
+    text-align: center;
+    background-color: rgba(0, 0, 0, 0.4);
+    color: white;
+    h2,
+    h3 {
+      margin: 0;
+      padding: 1px;
+    }
+
+    h2 {
+      font-size: 16px;
+      font-weight: 400;
+    }
+
+    h3 {
+      font-size: 14px;
+      font-weight: 300;
+    }
+  }
+}
+</style>
